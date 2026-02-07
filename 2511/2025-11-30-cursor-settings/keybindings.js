171e16b963508005843ffb60fb8614ef69f6d14b
ian <me@iany.me>
2026-02-07 13:02:37 +0800

    Update keybindings.js

--- 2511/2025-11-30-cursor-settings/keybindings.js
+++ 2511/2025-11-30-cursor-settings/keybindings.js
@@ -1,5 +1,25 @@
 [
   {
+    "key": "ctrl+shift+v",
+    "command": "-markdown.showPreview",
+    "when": "!notebookEditorFocused && editorLangId == 'markdown'"
+  },
+  {
+    "key": "ctrl+shift+v",
+    "command": "-planEditor.toggleMode",
+    "when": "markdownPlanEditorActive"
+  },
+  {
+    "key": "ctrl+shift+v",
+    "command": "-markdownEditor.toggleMode",
+    "when": "markdownEditorActive"
+  },
+  {
+    "key": "ctrl+shift+v",
+    "command": "-notebook.cell.pasteAbove",
+    "when": "notebookEditorFocused && !inputFocus"
+  },
+  {
     "key": "ctrl+/",
     "command": "-workbench.action.terminal.sendSequence",
     "when": "terminalFocus"
@@ -24,22 +44,23 @@
     "command": "-workbench.action.quickOpen"
   },
   {
-    "key": "ctrl+p",
-    "command": "-workbench.action.quickOpen"
-  },
-  {
     "key": "ctrl+alt+p",
     "command": "workbench.action.quickOpen"
   },
   {
-    "key": "ctrl+l",
+    "key": "ctrl+p",
+    "command": "-workbench.action.quickOpen"
+  },
+  {
+    "key": "ctrl+p",
     "command": "-expandLineSelection",
     "when": "textInputFocus"
   },
   {
     "key": "ctrl+l",
-    "command": "-aichat.newchataction"
-  }
+    "command": "-aichat.newchataction",
+    "when": "activeEditor != 'workbench.editor.browserEditor'"
+  },
   {
     "key": "ctrl+k",
     "command": "deleteAllRight",
